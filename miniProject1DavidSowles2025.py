f723512082d9dd47af390f2e7aaa57a6aa07c581
dsowles <d_sowles@mail.fhsu.edu>
2025-09-14 19:53:28 -0500

    Added required honesty message comment to main code file.

--- miniProject1DavidSowles2025.py
+++ miniProject1DavidSowles2025.py
@@ -6,6 +6,17 @@
 # Creation Date:    09/14/2025
 # Last Updated:     09/14/2025
 
+'''
+INF601 - Programming in Python
+Assignment miniProject1
+
+I,     David Sowles    , affirm that the work submitted for this assignment is entirely my own. 
+I have not engaged in any form of academic dishonesty, including but not limited to cheating, plagiarism, or the use of unauthorized materials. 
+I have neither provided nor received unauthorized assistance and have accurately cited all sources in adherence to academic standards. 
+I understand that failing to comply with this integrity statement may result in consequences, including disciplinary actions as determined 
+by my course instructor and outlined in institutional policies. By signing this statement, I acknowledge my commitment to upholding the principles of academic integrity.
+'''
+
 
 import numpy as np
 import yfinance as yf
