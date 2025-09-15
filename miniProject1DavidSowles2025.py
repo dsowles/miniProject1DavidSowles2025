946d84b2632a809c5b09f8275bede4a6210fe21b
dsowles <d_sowles@mail.fhsu.edu>
2025-09-14 19:41:06 -0500

    Project Initiation

---
+++ miniProject1DavidSowles2025.py
@@ -0,0 +1,9 @@
+# Filename:         miniProject1DavidSowles2025.py
+# Project:          MiniProject1
+# Author:           David A. Sowles
+# Creation Date:    09/14/2025
+# Last Updated:     09/14/2025
+
+
+
+print("foo")
